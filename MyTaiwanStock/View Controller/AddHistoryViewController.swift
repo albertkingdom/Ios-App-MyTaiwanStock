2504aef46ebb0f0955e573723d0b8e9567b31c55
albert <albertkingdom@gmail.com>
2022-08-31 11:03:30 +0800

    add function to add history vc

--- MyTaiwanStock/View Controller/AddHistoryViewController.swift
+++ MyTaiwanStock/View Controller/AddHistoryViewController.swift
@@ -13,11 +13,21 @@ class AddHistoryViewController: UITableViewController {
     var stockNo: String!
     
     let viewModel = AddHistoryViewModel()
-    
+    let feeCategory = ["折數","盤中零股", "自訂(元)"]
+    let feePercent = (1...10).map{ int -> String in
+        if int < 10 {
+            return "\(int)折"
+        } else {
+            return "無折扣"
+        }
+    }
+    var fee: Fee!
     @IBOutlet weak var stockNoLabel: UILabel!
     @IBOutlet weak var priceTextField: UITextField!
     @IBOutlet weak var amountTextField: UITextField!
     @IBOutlet weak var reasonTextView: UITextView!
+    @IBOutlet weak var feePicker: UIPickerView!
+    @IBOutlet weak var feeTextField: UITextField!
     
     @IBAction func setSegmentControl(_ sender: UISegmentedControl) {
 
@@ -45,16 +55,27 @@ class AddHistoryViewController: UITableViewController {
             let price = try validationService.validStockPriceInput(priceTextField.text)
             let amount = try validationService.validStockAmountInput(amountTextField.text)
             let reason = reasonTextView.text ?? ""
-            viewModel.saveNewRecord(
-                stockNo: stockNo,
-                price: price,
-                amount: amount,
-                reason: reason
-            )
-            showToast(message: "成功新增一筆投資紀錄")
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                self.navigationController?.popViewController(animated: true)
+            switch fee {
+            case .Percent(let percent):
+                print("fee percent \(percent)")
+            case .userDefined:
+                guard let str = feeTextField.text, let userDefinedFee = Int(str) else { return }
+                print("userDefinedFee \(userDefinedFee)")
+            case .OneDollar:
+                print("1")
+            default:break
             }
+            
+//            viewModel.saveNewRecord(
+//                stockNo: stockNo,
+//                price: price,
+//                amount: amount,
+//                reason: reason
+//            )
+//            showToast(message: "成功新增一筆投資紀錄")
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                self.navigationController?.popViewController(animated: true)
+//            }
         } catch {
             switch error {
             case ValidationError.invalidAmount:
@@ -90,6 +111,12 @@ class AddHistoryViewController: UITableViewController {
         
         priceTextField.delegate = self
         amountTextField.delegate = self
+        
+        feePicker.dataSource = self
+        feePicker.delegate = self
+        feePicker.selectRow(9, inComponent: 1, animated: true)
+        
+        feeTextField.delegate = self
     }
     
    
@@ -117,6 +144,97 @@ extension UIViewController {
 extension AddHistoryViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         textField.textColor = .label
+        
+        if textField.tag == 1 {
+            let strippedString = "$$"
+            print("range \(range),  string \(string)")
+//               // replace current content with stripped content
+//               if let replaceStart = textField.position(from: textField.beginningOfDocument, offset: range.location),
+//                   let replaceEnd = textField.position(from: replaceStart, offset: range.length),
+//                   let textRange = textField.textRange(from: replaceStart, to: replaceEnd) {
+//
+//                   textField.replace(textRange, withText: "\(textField.text) strippedString")
+//               }
+        }
         return true
     }
+}
+
+
+extension AddHistoryViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 3
+        }
+        if pickerView.selectedRow(inComponent: 0) == 1 {
+            return 1
+        }
+        if pickerView.selectedRow(inComponent: 0) == 2 {
+            return 1
+        }
+        return feePercent.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return feeCategory[row]
+        }
+        if pickerView.selectedRow(inComponent: 0) == 1 {
+            return "1元"
+        }
+        if pickerView.selectedRow(inComponent: 0) == 2 {
+            return "自訂"
+        }
+        
+        return feePercent[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //print("pickerview did select component \(component) row \(row)")
+        pickerView.reloadComponent(1)
+
+        switch pickerView.selectedRow(inComponent: 0) {
+        case 0:
+            // 折數
+            feeTextField.text = feePercent[row]
+            guard let priceStr = priceTextField.text,
+                  let amountStr = amountTextField.text,
+                  let priceFloat = Float(priceStr),
+                  let amountFloat = Float(amountStr)
+            else {return}
+            let multiplier = Float(row+1)*0.1
+            let calculatedFee = calFee(price: priceFloat, amount: amountFloat, multiplier: multiplier)
+            feeTextField.text = "\(calculatedFee) 元"
+            feeTextField.isEnabled = false
+            fee = Fee.Percent(multiplier)
+        case 1:
+            // 1元
+            feeTextField.text = "1元"
+            feeTextField.isEnabled = false
+            fee = Fee.OneDollar
+        case 2:
+            feeTextField.isEnabled = true
+            feeTextField.placeholder = "輸入手續費"
+            feeTextField.text = ""
+            fee = Fee.userDefined
+        default: break
+        }
+    }
+    
+    func calFee(price: Float, amount: Float, multiplier: Float) -> Float{
+        let total = price*amount*multiplier*0.001425
+        return total<20 ? 20 : total
+    }
+}
+
+enum Fee {
+    case Percent(Float)
+    case userDefined
+    case OneDollar
+    
 }
